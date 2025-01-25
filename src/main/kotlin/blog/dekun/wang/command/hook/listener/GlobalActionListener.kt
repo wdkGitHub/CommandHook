ce505b8fb66ab92560f18b0f4c1468d1d4e46620
wangdekun <wangdekunemail@gmail.com>
2025-01-25 11:53:26 +0800

    feat: 增加简陋的组搜索，过滤掉复制Action

--- src/main/kotlin/blog/dekun/wang/command/hook/listener/GlobalActionListener.kt
+++ src/main/kotlin/blog/dekun/wang/command/hook/listener/GlobalActionListener.kt
@@ -19,14 +19,39 @@ class GlobalActionListener : AnActionListener {
     override fun beforeActionPerformed(action: AnAction, event: AnActionEvent) {
         if (UnregisterActionService.isEnableNotify() && !action.javaClass.name.contains("dekun.wang")) {
             val actionId = ActionManager.getInstance().getId(action)
+            if ("EditorCopy" == actionId) {
+                return
+            }
             val actionName = action.templatePresentation.text
+            val groupId = getGroupId(actionName)
             val message = """
             ID = $actionId <br>
             Name = $actionName <br>
+            GroupId = $groupId <br>
             Class = ${action.javaClass.name}
             """.trimIndent()
-            blog.dekun.wang.command.hook.utils.Utils.showNotificationAnActionId("AnActionIdNotificationGroup", message)
+            blog.dekun.wang.command.hook.utils.Utils.showNotificationAnActionId(event.project, "AnActionIdNotificationGroup", message)
         }
+    }
+
+    fun getGroupId(actionText: String?): List<String> {
+        val mutableListOf = mutableListOf<String>()
+        actionText?.let {
+            ActionManager.getInstance().getActionIdList("").forEach { id ->
+                ActionManager.getInstance().getAction(id)?.let { action ->
+                    if (action is DefaultActionGroup) {
+                        action.getChildren(null).forEach {
+                            it.templatePresentation.text?.let { text ->
+                                if (text == actionText) {
+                                    mutableListOf.add(id)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return mutableListOf
     }
 
     override fun afterActionPerformed(action: AnAction, event: AnActionEvent, result: AnActionResult) {
